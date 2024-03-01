c66d63b20997fb3b657b36a6272a36a1e6cef81b
unknown <annemiehanson@gmail.com>
2024-03-01 21:21:56 +0000

    Generalised neighbour and related methods

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -6,6 +6,8 @@ public class Board
 {
     // Keep track of whether the game has started
     private boolean gameStarted = false;
+    // How many dimensions are we playing in
+    int boardDimensions = 2;
     private Tile[] contents = new Tile[10];
     // Keep track of how many mines remaining the player thinks there is
     private int minesToFind = 0;
@@ -13,7 +15,7 @@ public class Board
     // How many tiles can the player click on without exploding
     private int remainingSafeTiles = 0;
 
-    // dimensions stores x and y sizes
+    // store teh bounding box of our map
     private int[] boundingBox = new int[2];
 
     // Store the shape of a board with 'x' for an active tile and 'o' (or anything else) for an inactive tile
@@ -42,30 +44,114 @@ public class Board
         return contents.length - inactive;
     }
 
-    // Given a pair of coördinates, return the index they correspond to in this board.
+    // Given an array of coördinates, return the index they correspond to in this board.
     // If coördinates are not present on the board, return -1
     private int coordsToIndex(int[] coords)
     {
+        // If coords is empty, return 0
+        if ( coords.length == 0 )
+        {
+            return 0;
+        }
+
+        // If coords[0] is out of bounds, return -1
+        if ( coords[0] < 0 || coords[0] >= boundingBox[0] )
+        {
+                return -1;
+        }
+        // Remove the first element from coords for recursion and store in strippedCoords
+        int[] strippedCoords = new int[coords.length - 1];
+
+        for ( int i : strippedCoords )
+        {
+            strippedCoords[i] = coords[i + 1];
+        }
+
+        // Remove the first element from BoundingBox for recursion
+        int[] strippedBoundingBox = new int[boundingBox.length - 1];
+        for ( int i : strippedBoundingBox )
+        {
+            strippedBoundingBox[i] = boundingBox[i + 1];
+        }
+
+        // Return the appropriate index value, making sure that if it's negative, we set it to -1
+        return Math.max(coords[0] + (boundingBox[0] * coordsToIndex(strippedCoords)), -1);
+/*
         // Check if coördinates are out-of-bounds
         if (coords[0] < 0 || coords[0] >= boundingBox[0] || coords[1] < 0 || coords[1] >= boundingBox[1])
         {
             return -1;
         }
         // The first 0, 1, ..., x-1 tiles will be with y = 0, the next x, x+1, ..., 2x - 1 will be y = 2 etc.
-        return coords[1] * boundingBox[0] + coords[0];
+        return coords[1] * boundingBox[0] + coords[0];*/
+    }
+    private int coordsToIndex(int[] coords, int[] remBoundingBox)
+    {
+        // If coords is empty, return 0
+        if ( coords.length == 0 )
+        {
+            return 0;
+        }
+
+        // If coords[0] is out of bounds, return -1
+        if ( coords[0] < 0 || coords[0] >= boundingBox[0] )
+        {
+            return -1;
+        }
+        // Remove the first element from coords for recursion and store in strippedCoords
+        int[] strippedCoords = new int[coords.length - 1];
+
+        for ( int i : strippedCoords )
+        {
+            strippedCoords[i] = coords[i + 1];
+        }
+
+        // Remove the first element from remBoundingBox for recursion
+        int[] strippedBoundingBox = new int[remBoundingBox.length - 1];
+        for ( int i : strippedBoundingBox )
+        {
+                strippedBoundingBox[i] = remBoundingBox[i + 1];
+        }
+
+        // Return the appropriate index value, making sure that if it's negative, we set it to -1
+        return Math.max(coords[0] + (remBoundingBox[0] * coordsToIndex(strippedCoords)), -1);
     }
 
     // Given an index, return the coördinates they correspond to in this board, otherwise return [-1, -1]
     private int[] indexToCoords(int index)
     {
-        if ( index < 0 || index > contents.length)
+
+        // Create an array of size boardDimensions to store our coordinates in
+        int[] coords = new int[boardDimensions];
+
+        // If index is illegal, return a -1 array
+        if ( index < 0 || index >= contents.length)
+        {
+            for ( int i : coords )
+            {
+                coords[i] = -1;
+            }
+            return coords;
+        }
+
+        int strippedIndex = index;
+        for ( int i = 0;
+              i < coords.length;
+              ++i )
+        {
+            coords[i] = strippedIndex % boundingBox[i];
+            strippedIndex /= boundingBox[i];
+        }
+
+        return coords;
+        /*if ( index < 0 || index > contents.length)
         {
             return new int[]{-1, -1};
         }
         // The first 0, 1, ..., x-1 tiles will be with y = 0, the next x, x+1, ..., 2x - 1 will be y = 2 etc.
         int x = index % (boundingBox[0]);
         int y = index / (boundingBox[0]);
-        return new int[]{x, y};
+        return new int[]{x, y};*/
     }
 
     // If a tile has been revealed, update remainingSafeTiles and maybe win
@@ -173,25 +259,72 @@ public class Board
 
         return finalArray;
     }
+
+    private int pow (int number, int exponent)
+    {
+        int temp = 1;
+        for ( int i = 0;
+              i < exponent;
+              ++i )
+        {
+            temp *= number;
+        }
+
+        return temp;
+    }
+
+    // Generate an array of perturbations
+    private int[][] generatePerturbations(int[][] perturb, int dim)
+    {
+        // If dim == 0, we're done
+        if ( dim == 0 )
+        {
+            return perturb;
+        }
+
+        int[][] newPerturb = new int[pow(3,boardDimensions - dim + 1)][boardDimensions];
+
+        // For each perturbation already found, take it and add +1, -1 and 0 in the next dimension
+        for ( int i = 0;
+               i < perturb.length;
+               ++i )
+        {
+            int[] currPerturb = perturb[i];
+            currPerturb[boardDimensions - dim] = 1;
+            System.arraycopy(currPerturb, 0, newPerturb[3 * i], 0, currPerturb.length);
+            currPerturb[boardDimensions - dim] = -1;
+            System.arraycopy(currPerturb, 0, newPerturb[(3 * i) + 1], 0, currPerturb.length);
+            currPerturb[boardDimensions - dim] = 0;
+            System.arraycopy(currPerturb, 0, newPerturb[(3 * i) + 2], 0, currPerturb.length);
+        }
+
+        return generatePerturbations(newPerturb, dim - 1);
+    }
+
+    private int[][] generatePerturbations()
+    {
+        int[][] newPerturb = new int[1][boardDimensions];
+
+        // Generate all the unitary perturbations plus the 0 permutation
+        int[][] fullPerturb = generatePerturbations(newPerturb, boardDimensions);
+
+        // Return the unitary permutations with 0 stripped out
+        return Arrays.copyOfRange(fullPerturb, 0, fullPerturb.length - 1);
+    }
     // Given coordinates, return its 8 neighbours
     public Tile[] getNeighbours(int[] coords)
     {
-        int[] xPeturbation = new int[]{1, 0};
-        int[] xNegPeturbation = new int[]{-1, 0};
-        int[] yPeturbation = new int[]{0, 1};
-        int[] yNegPeturbation = new int[]{0, -1};
+        int[][] perturbations = generatePerturbations();
 
         ArrayList<Tile> neighbourList = new ArrayList<Tile>();
 
-        int[] indexArray = new int[8];
-        indexArray[0] = coordsToIndex(pairwiseSum(coords, xPeturbation));
-        indexArray[1] = coordsToIndex(pairwiseSum(coords, xNegPeturbation));
-        indexArray[2] = coordsToIndex(pairwiseSum(coords, yPeturbation));
-        indexArray[3] = coordsToIndex(pairwiseSum(coords, yNegPeturbation));
-        indexArray[4] = coordsToIndex(pairwiseSum(pairwiseSum(coords, xPeturbation), yPeturbation));
-        indexArray[5] = coordsToIndex(pairwiseSum(pairwiseSum(coords, xNegPeturbation), yPeturbation));
-        indexArray[6] = coordsToIndex(pairwiseSum(pairwiseSum(coords, xPeturbation), yNegPeturbation));
-        indexArray[7] = coordsToIndex(pairwiseSum(pairwiseSum(coords, xNegPeturbation), yNegPeturbation));
+        int[] indexArray = new int[pow(3, boardDimensions) - 1];
+        for ( int i = 0;
+              i < indexArray.length;
+              ++i )
+        {
+            indexArray[i] = coordsToIndex(pairwiseSum(coords, perturbations[i]));
+        }
 
         int arrayLength = 0;
         // Count the indices in bounds
